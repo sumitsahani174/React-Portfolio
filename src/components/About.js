27b4fad3fac85454c42d2e4f5c3f9386806a3cca
Sumit Sahani <sumitsahani174@gmail.com>
2025-07-31 21:32:09 +0530

    Latest Code

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
   <div className="section">
     <h2>About Me</h2>
-    <p>Motivated Full-Stack Developer with 5+ years of experience in developing scalable web applications and APIs using .NET Core and React.js. Proven expertise in healthcare and insurance domains, cloud-native development, and Agile methodologies.</p>
+    <p>Senior Motivated Full-Stack Developer with 5+ years of experience in developing scalable web applications and APIs using .NET Core and React.js. Proven expertise in healthcare and insurance domains, cloud-native development, and Agile methodologies.</p>
   </div>
 );
 
